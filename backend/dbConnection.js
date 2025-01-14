5d1a4f4a8f5cb416aa2a68a82dc8c5fba9ec1b38
Rashmika K R <krrashmika2004@gmail.com>
2025-01-14 13:31:12 +0530

    database url access

--- backend/dbConnection.js
+++ backend/dbConnection.js
@@ -14,7 +14,7 @@ const connectDB = async () => {
     });
     console.log("Database connected successfully");
   } catch (error) {
-    console.error("MongoDB connection error:", error);
+    console.error("MongoDB connection error: ", error);
     process.exit(1); // Exit process with failure
   }
 };
